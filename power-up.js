28e0a90ba1982f57bbef959c7c8a4496cfb93530
Alex <aralvarez023@gmail.com>
2025-02-12 12:10:24 -0500

    Update power-up.js

--- power-up.js
+++ power-up.js
@@ -1,1 +1,63 @@
+TrelloPowerUp.initialize({
+  'card-buttons': function(t, options) {
+    return [{
+      icon: 'https://cdn-icons-png.flaticon.com/512/992/992651.png',
+      text: 'Set Story Points',
+      callback: function(t) {
+        return t.popup({
+          title: 'Set Story Points',
+          url: 'story-points.html'
+        });
+      }
+    }, {
+      icon: 'https://cdn-icons-png.flaticon.com/512/992/992651.png',
+      text: 'Set Parent Card',
+      callback: function(t) {
+        return t.popup({
+          title: 'Set Parent Card',
+          url: 'set-parent.html'
+        });
+      }
+    }];
+  },
+
+  'card-badges': function(t, options) {
+    return t.get('card', 'shared', 'storyPoints')
+      .then(function(points) {
+        if (points) {
+          return [{
+            text: points + ' SP',
+            color: 'blue'
+          }];
+        }
+        return [];
+      });
+  },
+
+  'card-detail-badges': function(t, options) {
+    return t.get('card', 'shared', 'parentCard')
+      .then(function(parentId) {
+        if (parentId) {
+          return [{
+            title: 'Parent Card',
+            text: 'Linked',
+            callback: function(t) {
+              return t.board('id', 'name').then(function(board) {
+                window.open(`https://trello.com/c/${parentId}`, '_blank');
+              });
+            }
+          }];
+        }
+        return [];
+      });
+  },
+
+  'on-enable': function(t, options) {
+    console.log('Power-Up enabled');
+  },
+
+  'on-disable': function(t, options) {
+    console.log('Power-Up disabled');
+  }
+});
 
